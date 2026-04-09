4200e32e875d69ff25b38acbff92966bd9e72621
GNC3842 <antonydemacia@gmail.com>
2026-04-09 20:34:11 +0200

    up: lorentz system

--- simulation/system.py
+++ simulation/system.py
@@ -6,5 +6,93 @@ import numpy as np
 from dataclasses import dataclass, field
 
 ################
-#  Classe de base
+#  Classe de base abstraite d'un attracteur
 ################
+class ChaoticSysteme:
+  """
+  Interface abstraite.  Tout attracteur doit implémenter :
+    - deriv(state)       → vecteur dérivée
+    - default_state      → conditions initiales canoniques
+    - name               → nom lisible
+  """
+  
+  name: str = "AbstractSystem"
+
+  def deriv(self,state):
+    raise NotImplementedError
+  
+  @property
+  def default_state(self):
+    raise NotImplementedError
+
+
+################
+# Attracteur de Lorentz
+################
+class LorentzSystem(ChaoticSysteme):
+  """
+  Régi par 3 équations:
+      dx/dt = σ(y - x)
+      dy/dt = x(ρ - z) - y
+      dz/dt = xy - βz
+  
+  Ces 3 grandeurs physiques sont:
+      x -> intensité de convection
+      y -> différence de température ascendant/descdenat
+      z -> distorsion du profil thermique vertical
+  """
+  sigma: float = 10.0
+  rho: float = 28.0
+  beta: float = 8.0/3.0
+  name: str   = field(default="Lorenz", init=False, repr=False)
+
+  def deriv(self,state):
+    """Calcule des dérivées dx/dt, dy/dt et dz/dt
+
+    Args:
+        state (ndarray):  derive des vecteurs ie state = [x,y,z] ou bien state = [[x1,y1,z1],
+                                                                                  [x2,y2,z2]
+                                                                                  [x3,y3,z3],...]
+    Return: 
+        dérivées de meme forme que state
+    """
+    x = state[...,0]  #
+    y = state[...,1]
+    z = state[...,2]
+
+    dx = self.sigma*(y - x)
+    dy = x*(self.rho - z) - y
+    dz = x*y - self.beta*z
+
+    return np.stack([dx, dy, dz], axis=-1)
+
+  @property
+  def default_state(self):
+    return np.array([0.1, 0.0, 0.0])
+  
+  @property
+  def fixed_points(self) -> list[np.ndarray]:
+    """
+    Points fixes analytiques du système de Lorenz.
+    Utiles pour la section de Poincaré (plan z = rho - 1).
+ 
+    Pour ρ > 1 :  C± = (±√(β(ρ−1)),  ±√(β(ρ−1)),  ρ−1)
+    """
+
+    if self.rho < 1:
+      return [np.zeros(3)]
+    val = np.sqrt(self.beta * (self.rho - 1))
+    return [np.array([ val,  val, self.rho - 1]),
+            np.array([-val, -val, self.rho - 1]),]
+
+  @property
+  def poincare_z(self):
+    """Plan de coupe canonique pour la section de Poincaré."""
+    return self.rho - 1.0
+
+
+################
+# Attracteur de Rössler
+################
+class RosslerSystem(ChaoticSysteme):
+  pass

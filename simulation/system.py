127ad12af67f25465cf4bafc550e3157fd6b699e
GNC3842 <antonydemacia@gmail.com>
2026-04-09 19:43:51 +0200

    base

---
+++ simulation/system.py
@@ -0,0 +1,10 @@
+#Definition des systèmes dynamiques chaotiques
+
+
+from __future__ import annotations
+import numpy as np
+from dataclasses import dataclass, field
+
+################
+#  Classe de base
+################
